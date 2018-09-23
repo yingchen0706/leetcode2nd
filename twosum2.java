54753f7137a7b968f85b13c7bc978cbf537b1aff
yingchen0706 <yingchen0706@users.noreply.github.com>
2018-09-24 07:35:17 +0800

    Create twosum2.java

---
+++ twosum2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int i = 0, j = nums.length -1;
+        while(i<j) {
+            int sum = nums[i] + nums[j];
+            if (sum == target) {
+                return new int[]{i+1, j+1};
+            } else if (sum > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
