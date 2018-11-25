b269a1680add6fbb2f178b99c7cf0e3f14d49a0a
yingchen0706 <yingchen0706@users.noreply.github.com>
2018-11-25 23:13:44 +0800

    Create 9palindromnumber.java

---
+++ 9palindromnumber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x %10 == 0 && x != 0)) {
+            return false;
+        }
+        
+        int revertNum = 0;
+        while (x > revertNum) {
+            revertNum = revertNum*10+x%10;
+            x = x/10;
+        }
+        
+        return x == revertNum || x == revertNum/10;
+    }
+}
