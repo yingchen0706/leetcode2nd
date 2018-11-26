d59ce8f5f3c53d1b28aedf2d6b409046c548f17a
yingchen0706 <yingchen0706@users.noreply.github.com>
2018-11-26 23:34:04 +0800

    Create 11containmostwater2.java

---
+++ 11containmostwater2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length - 1;
+        int max = Integer.MIN_VALUE;
+        while (i<j) {
+            max = Math.max(max, Math.min(height[i], height[j])*(j-i));
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return max;
+    }
+}
